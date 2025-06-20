3aafbd0222257a7151b7439b5a226b4e86363659
Azuyah <matseddieandersson@gmail.com>
2025-06-20 12:10:24 +0200

    Added Railworker logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,4 @@
 // Header.js
-
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
@@ -9,6 +8,7 @@ import {
   Button,
   HStack,
   useColorModeValue,
+  Image,
 } from '@chakra-ui/react';
 
 const Header = () => {
@@ -19,49 +19,55 @@ const Header = () => {
     navigate('/');
   };
 
-  return (
-    <Box
-      bg="blue.700"
-      color="white"
-      px={6}
-      py={4}
-      position="fixed"
-      top={0}
-      left={0}
-      width="100%"
-      zIndex={1000}
-      boxShadow="md"
-    >
-      <Flex maxW="7xl" mx="auto" align="center" justify="space-between">
+return (
+  <Box
+    bg="blue.700"
+    color="white"
+    px={6}
+    py={4}
+    position="fixed"
+    top={0}
+    left={0}
+    width="100%"
+    zIndex={1000}
+    boxShadow="md"
+  >
+    <Flex maxW="7xl" mx="auto" align="center" justify="space-between">
+      {/* Vänster sida: logga + text */}
+      <Flex align="center" gap={3}>
+        <Image src="/railworkerlogo.svg" alt="Railworker logo" boxSize="40px" />
         <Heading size="lg" letterSpacing="wide">
           Railworker
         </Heading>
-        <HStack spacing={4}>
-          <Button
-            colorScheme="whiteAlpha"
-            variant="solid"
-            onClick={() => navigate('/dashboard')}
-          >
-            Start
-          </Button>
-          <Button
-            colorScheme="whiteAlpha"
-            variant="solid"
-            onClick={() => navigate('/profil')}
-          >
-            Min profil
-          </Button>
-          <Button
-            colorScheme="red"
-            variant="solid"
-            onClick={logout}
-          >
-            Logga ut
-          </Button>
-        </HStack>
       </Flex>
-    </Box>
-  );
+
+      {/* Höger sida: knappar */}
+      <HStack spacing={4}>
+        <Button
+          colorScheme="whiteAlpha"
+          variant="solid"
+          onClick={() => navigate('/dashboard')}
+        >
+          Start
+        </Button>
+        <Button
+          colorScheme="whiteAlpha"
+          variant="solid"
+          onClick={() => navigate('/profil')}
+        >
+          Min profil
+        </Button>
+        <Button
+          colorScheme="red"
+          variant="solid"
+          onClick={logout}
+        >
+          Logga ut
+        </Button>
+      </HStack>
+    </Flex>
+  </Box>
+);
 };
 
 export default Header;
