377ff0a047be7afef8f4561f86c3a0f1eb07ece6
Azuyah <matseddieandersson@gmail.com>
2025-06-20 09:40:17 +0200

    Added functioning user login

--- backend/index.js
+++ backend/index.js
@@ -93,13 +93,6 @@ app.post('/api/login', async (req, res) => {
   });
 res.cookie('token', token, {
   httpOnly: true,
-  secure: true,
-  sameSite: 'None',
-  maxAge: 7 * 24 * 60 * 60 * 1000 // 7 dagar
-});
-
-res.cookie('token', token, {
-  httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
   sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
   maxAge: 7 * 24 * 60 * 60 * 1000
